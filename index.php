5b3ed8d51861ada3fd11c1c0c26a2e8d26b72fe1
Tyler Merrill <tmerrill@rk03-a1002-18.francistuttle.edu>
2015-11-05 08:32:59 -0600

    Switching index page to PHP.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-widthm initial-scale=1.0">
+<title>MBAT Car Safety -- Home</title>
+<link rel="stylesheet" href="css/normalize.css">
+</head>
+<body>
+   <header role="banner">
+
+   </header>
+
+   <main role="main">
+
+   </main>
+
+   <footer>
+
+   </footer>
+</body>
+
+</html>
