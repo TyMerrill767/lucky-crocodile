918adde08c34c00a3f87850b5dceccfb4bfb83cd
Tyler Merrill <tmerrill@rk03-a1002-18.francistuttle.edu>
2015-11-05 08:32:06 -0600

    Adding page for prevention information.

---
+++ prevention.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-widthm initial-scale=1.0">
+<title>MBAT Car Safety -- Dangers of Leaving Your Child Unattended in a Vehicle</title>
+<link rel="stylesheet" href="css/normalize.css">
+</head>
+<body>
+   <header role="banner">
+
+   </header>
+
+   <main role="main">
+
+   </main>
+
+   <footer>
+
+   </footer>
+</body>
+
+</html>
