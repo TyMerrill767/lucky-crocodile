2ef8c8edfc81b3abdc291923d27110059ea0ede6
Tyler Merrill <tmerrill@rk03-a1002-18.francistuttle.edu>
2015-11-05 09:02:15 -0600

    Adding links for index.php, dangers.php and prevention.php.

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,8 @@
-<nav role="navigation"></nav>
+<nav role="navigation">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="dangers.php">Dangers</a></li>
+        <li><a href="prevention.php">Prevention</a></li>
+        <li><a href=""></a></li>
+    </ul>
+</nav>
