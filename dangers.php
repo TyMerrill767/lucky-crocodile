6fae4f054b2fa3c5c84580efc29d756f300eb079
Tyler Merrill <tmerrill@rk03-a1002-18.francistuttle.edu>
2015-11-06 09:49:21 -0600

    Adding placeholder text and images to pages.

--- dangers.php
+++ dangers.php
@@ -12,7 +12,8 @@
    </header>
 
    <main role="main">
-
+        <p>Ut lectus tellus, tempus a sem vel, lobortis fringilla massa. Morbi vehicula tincidunt massa. Sed ullamcorper sapien eget ipsum aliquet, eu fermentum lacus accumsan. Quisque semper lectus ut ultrices placerat. Cras sit amet ultrices nulla. Nulla felis nulla, faucibus laoreet elit sit amet, porttitor malesuada justo. Proin at metus viverra, sodales tortor rhoncus, euismod sem. Donec augue felis, tempus ut ligula id, lobortis fringilla odio. Quisque orci orci, cursus nec tincidunt at, aliquet sed diam. Suspendisse bibendum egestas turpis sit amet sagittis. Sed ullamcorper commodo iaculis. Quisque nec tristique arcu. Nam bibendum arcu a lacus volutpat, eu rhoncus nulla vehicula. Mauris tortor sem, tristique sit amet rhoncus eget, bibendum vitae orci. Integer sit amet ante in ipsum faucibus pharetra eu non quam. Quisque eget blandit neque. </p>
+        <img src="images/78_2536958.JPG" alt="placeholder">
    </main>
 
    <footer>
--- index.php
+++ index.php
@@ -12,7 +12,8 @@
    </header>
 
    <main role="main">
-
+        <img src="images/78_2536958.JPG" alt="placeholder">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt augue quis velit dignissim, ac congue magna commodo. In in urna imperdiet mauris congue scelerisque non sit amet enim. Nulla arcu magna, vehicula nec magna sit amet, tempus tristique elit. Nulla id lectus maximus, iaculis lacus sed, tincidunt arcu. Integer ac sapien justo. Etiam feugiat nibh vel elit varius, sed venenatis nulla euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus venenatis felis at odio tempus, efficitur egestas nibh egestas. Praesent tincidunt, ante vitae aliquet aliquet, ligula turpis ultricies elit, at congue mauris tellus quis est. Ut laoreet ut urna sit amet tincidunt.</p>
    </main>
 
    <footer>
