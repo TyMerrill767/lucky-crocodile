8c2d965de18dd1d98baec987da9bb7c1c32632e5
Tyler Merrill <tmerrill@rk03-a1002-18.francistuttle.edu>
2015-11-06 10:14:10 -0600

    Adding link to action.php.

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,6 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="dangers.php">Dangers</a></li>
         <li><a href="prevention.php">Prevention</a></li>
-        <li><a href=""></a></li>
+        <li><a href="action.php">Take Action</a></li>
     </ul>
 </nav>
