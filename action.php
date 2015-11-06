1b9370e27a4f60238fe352f1393b66b56a569caf
Tyler Merrill <tmerrill@rk03-a1002-18.francistuttle.edu>
2015-11-06 09:38:59 -0600

    Adding "Take Action" page.

---
+++ action.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-widthm initial-scale=1.0">
+<title>MBAT Car Safety -- Take Action</title>
+<link rel="stylesheet" href="css/normalize.css">
+</head>
+<body>
+   <header role="banner">
+        <?php include('includes/header.php');?>
+   </header>
+
+   <main role="main">
+
+   </main>
+
+   <footer>
+        <?php include('includes/footer.php');?>
+   </footer>
+</body>
+
+</html>
