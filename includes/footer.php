b3a82742e080b3ce27ccea4e302c4a5223add75a
Tyler Merrill <tmerrill@rk03-a1002-18.francistuttle.edu>
2015-11-12 10:14:27 -0600

    Adding initializing script for SlickNav to footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,7 @@
 <script src="js/jquery.min.js"></script>
 <script src="js/jquery.slicknav.min.js"></script>
+
+<!--Initializes responsive menu functionality-->
 <script>
 	$(function(){
 		$('#menu').slicknav();
