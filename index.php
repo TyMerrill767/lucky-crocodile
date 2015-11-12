cbf57a82736838483dc3f85a7f047aea04558858
Tyler Merrill <tmerrill@rk03-a1002-18.francistuttle.edu>
2015-11-12 09:28:56 -0600

    Changing pages to reference style.css.

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-widthm initial-scale=1.0">
+<meta name="viewport" content="width=device-width initial-scale=1.0">
 <title>MBAT Car Safety -- Home</title>
-<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
    <header role="banner">
