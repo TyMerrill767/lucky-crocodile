eb78d0b28d61b286c44d05cf5aaf7c6f5ef33efe
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-10 09:03:36 -0600

    Adding images and placeholder text to page.

--- dangers.php
+++ dangers.php
@@ -10,8 +10,12 @@
    </header>
 
    <main role="main">
+        <img src="images/interior1.png" alt="Car Interior 1">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consequat ipsum vel porta imperdiet. Aliquam egestas mollis fermentum. Proin ac odio sit amet quam cursus lacinia. Fusce eleifend vitae metus vel ultricies. Duis non neque vitae diam lobortis tempus non ac orci. Nulla diam eros, maximus in lorem quis, consectetur tincidunt eros. In vel nisl a enim mollis porta. Pellentesque felis orci, sagittis non congue sed, dictum nec ligula. Sed at mauris vel lacus fermentum eleifend. Nullam dignissim sem a elit fringilla, quis sagittis eros eleifend. Nullam non nisi nisi. Quisque ac laoreet ligula, quis blandit dolor. Vestibulum nec bibendum eros, sit amet viverra tortor. Nunc eu urna in urna ullamcorper consectetur sit amet quis eros. Ut nec ultrices enim. Praesent a justo auctor, bibendum massa at, consequat turpis.</p>
+        <img src="images/interior2.png" alt="Car Interior 2">
+        <p>Quisque lobortis felis at tincidunt condimentum. Proin vitae lectus in elit aliquam semper ac a elit. Vivamus ut turpis dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mattis tellus sed nulla maximus varius. Nam scelerisque in massa et bibendum. Maecenas sed nibh turpis. Quisque suscipit at lacus eget sagittis. Vivamus ut finibus felis, sed ultrices nisi. Suspendisse egestas felis vel massa rhoncus, et congue lacus rhoncus. Cras at nibh lobortis, placerat odio vel, tempor nisi. Sed eu sapien est. Quisque ornare posuere augue, quis accumsan justo condimentum in. Cras vel vulputate dolor. Cras auctor tincidunt massa, in lobortis orci ultrices et.</p>
+        <img src="images/interior3.png" alt="Car Interior 3">
         <p>Ut lectus tellus, tempus a sem vel, lobortis fringilla massa. Morbi vehicula tincidunt massa. Sed ullamcorper sapien eget ipsum aliquet, eu fermentum lacus accumsan. Quisque semper lectus ut ultrices placerat. Cras sit amet ultrices nulla. Nulla felis nulla, faucibus laoreet elit sit amet, porttitor malesuada justo. Proin at metus viverra, sodales tortor rhoncus, euismod sem. Donec augue felis, tempus ut ligula id, lobortis fringilla odio. Quisque orci orci, cursus nec tincidunt at, aliquet sed diam. Suspendisse bibendum egestas turpis sit amet sagittis. Sed ullamcorper commodo iaculis. Quisque nec tristique arcu. Nam bibendum arcu a lacus volutpat, eu rhoncus nulla vehicula. Mauris tortor sem, tristique sit amet rhoncus eget, bibendum vitae orci. Integer sit amet ante in ipsum faucibus pharetra eu non quam. Quisque eget blandit neque. </p>
-        <img src="images/78_2536958.JPG" alt="placeholder">
    </main>
 
    <footer>
