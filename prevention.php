dd5b7e5199c70c6c26fd4efa105ad57852a72b58
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-10 13:10:22 -0600

    Removing placeholder text and adding real content.

--- prevention.php
+++ prevention.php
@@ -10,9 +10,41 @@
    </header>
 
    <main role="main">
+        <h1>Prevention and Precautions</h1>
+        <p>So now that you know the dangers, what can you do to prevent tragedy?</p>
+
         <img src="images/78_2536958.JPG" alt="placeholder">
-        <p>Ut lectus tellus, tempus a sem vel, lobortis fringilla massa. Morbi vehicula tincidunt massa. Sed ullamcorper sapien eget ipsum aliquet, eu fermentum lacus accumsan. Quisque semper lectus ut ultrices placerat. Cras sit amet ultrices nulla. Nulla felis nulla, faucibus laoreet elit sit amet, porttitor malesuada justo. Proin at metus viverra, sodales tortor rhoncus, euismod sem. Donec augue felis, tempus ut ligula id, lobortis fringilla odio. Quisque orci orci, cursus nec tincidunt at, aliquet sed diam. Suspendisse bibendum egestas turpis sit amet sagittis. Sed ullamcorper commodo iaculis. Quisque nec tristique arcu. Nam bibendum arcu a lacus volutpat, eu rhoncus nulla vehicula. Mauris tortor sem, tristique sit amet rhoncus eget, bibendum vitae orci. Integer sit amet ante in ipsum faucibus pharetra eu non quam. Quisque eget blandit neque. </p>
+
+        <section>
+        <h2>Inside the Car</h2>
+        <ul>
+            <li>Clean your car regularly – especially of small objects such as trinkets, toys, or stray food items that may have fallen into hidden spaces; be sure to look under seats and in storage areas</li>
+            <li>Choose foods that are easily chewed and swallowed to prevent choking, or cut foods into small pieces and remove things such as seeds and pits, and never leave a child unattended while eating</li>
+            <li>Place the child’s car seat so it is not directly behind the driver’s seat</li>
+            <li>Place a visual cue in the front seat, such as a diaper bag or stuffed animal, to remind yourself that the child is in the back seat</li>
+        </ul>
+       </section>
+
         <img src="images/78_2536958.JPG" alt="placeholder">
+
+        <section>
+            <h2>Outside the Car</h2>
+            <ul>
+                <li>Always bring your child with you when you exit the vehicle – no amount of time is safe to leave your child unattended in the vehicle, even for “just a minute”, and it is illegal to do so in some states</li>
+                <li>Be sure that you lock the vehicle’s doors and trunk after exiting with your child – even when parked and at home, a vehicle can endanger a child, as they can become trapped within it if left open</li>
+                <li>Instruct children that the vehicle is not a safe place to play</li>
+            </ul>
+       </section>
+
+       <section>
+           <h2>In General</h2>
+           <ul>
+               <li>Familiarize yourself with life-saving techniques such as the Heimlich Maneuver, child cardiopulminary resuscitation (CPR), and other first-aid measures, and call 911 for emergencies</li>
+               <li>Be sure your child's caretaker(s) are aware of the dangers and solutions, and always have a plan of action in case of emergency</li>
+               <li>Be aware of your child's behavior and condition: if they are acting strange or confused, or if they are exhibiting nausea, dizziness, sudden cessation of sweating, or quick/slow pulse, they may be suffering heat stroke</li>
+           </ul>
+       </section>
+
    </main>
 
    <footer>
