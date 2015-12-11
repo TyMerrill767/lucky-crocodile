6fb7081b2c30b598dfca3657ccd8557c5b61f1ea
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-11 13:23:28 -0600

    Removed placeholder image, inserted image intended for page.

--- index.php
+++ index.php
@@ -10,10 +10,15 @@
    </header>
 
    <main role="main">
-        <img src="images/78_2536958.JPG" alt="placeholder">
+
         <h1>Toddler Neglect in Vehicles</h1>
+
         <p>Each year, an average of 35 children die due to vehicular heat stroke. Over 100 die due to back- and front-overs. Others die from the vehicle accidentally being set in motion, strangulation by power windows, choking on small objects, and more. One by one, these young lives were ended before their time. But what makes them especially tragic is that each and every one of them was preventable.</p>
+
+        <img src="images/child.png" alt="Child">
+
         <p>It isn't a matter of being a good or bad parent, or being an irresponsible person; all it takes is a momentary lapse of judgment on the part of an otherwise loving caretaker to endanger the life of a small child in a vehicle. This is because there are so many dangers lurking in and around a vehicle, both obvious and hidden, that can claim a child's life before one has time to realize it.</p>
+
         <p>So how can you avoid these tragedies? Read on to learn about the dangers and solutions to leaving a child alone in a vehicle, and how you can take action to avoid repeating these unfortunate mistakes before they happen.</p>
    </main>
 
