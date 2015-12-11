f168835078d5db75a674fff9dbcc8ba8e687138b
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-11 10:45:42 -0600

    Removing placeholder content, adding real content.

--- index.php
+++ index.php
@@ -11,9 +11,13 @@
 
    <main role="main">
         <img src="images/78_2536958.JPG" alt="placeholder">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt augue quis velit dignissim, ac congue magna commodo. In in urna imperdiet mauris congue scelerisque non sit amet enim. Nulla arcu magna, vehicula nec magna sit amet, tempus tristique elit. Nulla id lectus maximus, iaculis lacus sed, tincidunt arcu. Integer ac sapien justo. Etiam feugiat nibh vel elit varius, sed venenatis nulla euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus venenatis felis at odio tempus, efficitur egestas nibh egestas. Praesent tincidunt, ante vitae aliquet aliquet, ligula turpis ultricies elit, at congue mauris tellus quis est. Ut laoreet ut urna sit amet tincidunt.</p>
+        <h1>Toddler Neglect in Vehicles</h1>
+        <p>Each year, an average of 35 children die due to vehicular heat stroke. Over 100 die due to back- and front-overs. Others die from the vehicle accidentally being set in motion, strangulation by power windows, choking on small objects, and more. One by one, these young lives were ended before their time. But what makes them especially tragic is that each and every one of them was preventable.</p>
+        <p>It isn't a matter of being a good or bad parent, or being an irresponsible person; all it takes is a momentary lapse of judgment on the part of an otherwise loving caretaker to endanger the life of a small child in a vehicle. This is because there are so many dangers lurking in and around a vehicle, both obvious and hidden, that can claim a child's life before one has time to realize it.</p>
+        <p>So how can you avoid these tragedies? Read on to learn about the dangers and solutions to leaving a child alone in a vehicle, and how you can take action to avoid repeating these unfortunate mistakes before they happen.</p>
    </main>
 
+   <a href="dangers.php">Dangers of leaving your child in a vehicle >></a>
    <footer>
         <?php include('includes/footer.php');?>
    </footer>
