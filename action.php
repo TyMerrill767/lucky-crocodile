d453edf2a3c9acb094484d454ed68a744723cdf4
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-11 13:22:56 -0600

    Replaced placeholder image with image intended for page.

--- action.php
+++ action.php
@@ -10,8 +10,8 @@
    </header>
 
    <main role="main">
+        <img src="images/take_action.png" alt="">
         <p>Fusce consequat ut diam et dapibus. Etiam tincidunt ex vitae tortor pharetra, eu tincidunt lectus facilisis. Nunc ac diam eget justo venenatis ornare. Mauris molestie nisl id ipsum pretium vehicula. Duis semper laoreet urna ut sollicitudin. Curabitur est nisl, finibus ut facilisis ut, congue at arcu. Morbi vitae odio eget lectus commodo fringilla. Etiam laoreet orci enim. Nunc elementum eros efficitur pretium convallis. Aenean ut odio eget odio mollis vehicula vel a nisi. Ut nec turpis lacinia, bibendum urna id, lobortis nulla. Duis tristique lacus nulla, non luctus nisl dapibus id. Donec eget justo nec tortor lacinia ultricies ac vitae lorem. Aenean aliquet ante a sem sollicitudin finibus. Sed aliquam sem in finibus tempus. Vestibulum accumsan porttitor sapien, semper iaculis mi efficitur ut. </p>
-        <img src="images/78_2536958.JPG" alt="placeholder">
    </main>
 
    <footer>
