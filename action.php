cd80ea1875ff6331fabda9a9ff4e8f0241f97f98
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-14 15:09:08 -0600

    Updating content.

--- action.php
+++ action.php
@@ -11,9 +11,13 @@
 
    <main role="main">
         <img src="images/take_action.png" alt="Three Circles Child Safety">
-        <p>As someone with a personal connection to the field of child vehicle safety, be it through your own experience or that of a loved one, you are in a unique position to offer hope, understanding and vital information to others. At the same time, it is important to carefully consider the potential impact of your message on the community, as well the effects of self-disclosure on your personal well-being and for the well being of your children.</p>
+        <p>As someone with a personal connection to the field of child vehicle safety, be it through your own experience or that of a loved one, you are in an exclusive position to offer hope, understanding and crucial information to others. At the same time, it is important to carefully consider the potential impact of your message on the community, as well the effects of self-discovery on your personal well-being and for the well being of your children. </p>
 
         <p>In the world of parenting, danger awaits around every corner. You have to be willing to put in the effort to protect your children from it. You are their guardian, their protector, YOU are responsible for what happens to your child so use what you now know about child vehicle safety and make a difference.</p>
+
+        <p>If you witness something that doesn’t follow child vehicle safety protocol do something. For example, if you witness someone lock their infant in the car while they go run errands notify the proper authorities immediately and proceed to break the window opposite of the infant to get it out. Be sure to wait for the authorities to arrive while you hold the infant. Always make sure to lock your car and keep your keys out of reach of children.</p>
+
+        <p>Too many tragedies happen every year that can easily be prevented if the parties involved know proper child vehicle safety precautions. Everyone should have at the very least a basic understanding of child vehicle safety. It’s encouraged that you inform your friends and family with children about proper child vehicle safety and inform them of the risks that surround their vehicles. Who knows, you may just save a life.</p>
    </main>
 
    <footer>
