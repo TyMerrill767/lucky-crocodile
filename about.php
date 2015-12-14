3c30ed32a8253205a36c7a0c6b589c91bfa2cbcd
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-14 08:46:16 -0600

    Adding team member photo images to page, as well as headings.

--- about.php
+++ about.php
@@ -10,24 +10,27 @@
    </header>
 
    <main role="main">
+        <h1>About Us</h1>
 
+        <h2>BPA Information:</h2>
         <p>Chapter: </p>
         <p>Theme: Toddler Neglect in Vehicles</p>
         <p>School: Francis Tuttle Technology Center</p>
         <p>City: Oklahoma City, OK</p>
         <p>Year: 2015</p>
 
-        <img src="" alt="Tyler Picture">
-        <p>Tyler Merrill</p>
+        <img src="images/tyler_sm.png" alt="Tyler Picture">
+        <h3>Tyler Merrill</h3>
 
-        <img src="" alt="Brian Picture">
-        <p>Brian Richardson</p>
+        <img src="images/brian_sm.png" alt="Brian Picture">
+        <h3>Brian Richardson<h3>
+        <p></p>
 
-        <img src="" alt="Abigail Picture">
-        <p>Abigail Duran</p>
+        <img src="images/abi_sm.png" alt="Abigail Picture">
+        <h3>Abigail Duran</h3>
 
-        <img src="" alt="Matthew Picture">
-        <p>Matthew Sterling</p>
+        <img src="images/matt_sm.png" alt="Matthew Picture">
+        <h3>Matthew Sterling</h3>
    </main>
 
    <footer>
