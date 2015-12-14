1736a45b93f6ee2dd0e75142215084ed15de88cc
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-14 13:19:00 -0600

    Adding animated navigation icons.

--- includes/header.php
+++ includes/header.php
@@ -2,20 +2,20 @@
 
 <nav role="navigation" id="menu">
     <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="dangers.php">Dangers</a></li>
-        <li><a href="prevention.php">Prevention</a></li>
-        <li><a href="action.php">Take Action</a></li>
-        <li><a href="about.php">About Us</a></li>
+        <li><a href="index.php" id="homeNav">Home</a></li>
+        <li><a href="dangers.php" id="dangerNav">Dangers</a></li>
+        <li><a href="prevention.php" id="prevNav">Prevention</a></li>
+        <li><a href="action.php" id="actionNav">Take Action</a></li>
+        <li><a href="about.php" id="aboutNav">About Us</a></li>
     </ul>
 </nav>
 
 <div id="owl-demo" class="owl-carousel owl-theme">
 
-  <div class="item"><img src="images/coffee.png" alt=""></div>
-  <div class="item"><img src="images/milk.png" alt=""></div>
-  <div class="item"><img src="images/money.png" alt=""></div>
-  <div class="item"><img src="images/Rx.png" alt=""></div>
-  <div class="item"><img src="images/wine.png" alt=""></div>
+  <div class="item"><img src="images/coffee.png" alt="Slide"></div>
+  <div class="item"><img src="images/milk.png" alt="Slide"></div>
+  <div class="item"><img src="images/money.png" alt="Slide"></div>
+  <div class="item"><img src="images/Rx.png" alt="Slide"></div>
+  <div class="item"><img src="images/wine.png" alt="Slide"></div>
 
 </div>
