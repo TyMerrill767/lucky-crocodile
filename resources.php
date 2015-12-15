a2c7b4122599788bc60ac16febc27be9b69b8c53
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-15 14:53:58 -0600

    Adding resources page.

---
+++ resources.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>D.O.N.T. -- Resources</title>
+<?php include('includes/head.php');?>
+</head>
+<body>
+   <header role="banner">
+        <?php include('includes/header.php');?>
+   </header>
+
+   <main role="main">
+        <p>Help us spread the word!</p>
+
+        <p>Many of our images are available as posters for you to order. These posters are great
+for daycare facilities, churches, and other venues where children are likely to be
+present with their caregivers.</p>
+
+        <p>We sell each kit for the low price of $29.99 and the proceeds go further fund the efforts
+of D.O.N.T.</p>
+
+        <p>&ldquo;What Would You Trade&rdquo; Posters</p>
+        <img src="images/psa_posters.png" alt="6 Posters">
+        <ul>
+            <li>Kit of 6 Posters</li>
+            <li>30&rdquo; X 30 &rdquo;</li>
+            <li>Printed on high quality glossy stock</li>
+        </ul>
+
+        <p>Vehicle Interior Posters</p>
+        <img src="images/interior_posters.png" alt="Interior 3 Posters">
+        <ul>
+            <li>Kit of 3 infographic posters</li>
+            <li>18&rdquo; X 24&rdquo;</li>
+            <li>Printed on high quality glossy stock</li>
+        </ul>
+
+        <p>Wall Icons</p>
+        <img src="images/icon_posters.png" alt="Icon 7 Posters">
+        <ul>
+            <li>Kit of 7</li>
+            <li>12&rdquo; X 12&rdquo;</li>
+            <li>Printed on card stock with adhesive tabs on back</li>
+        </ul>
+   </main>
+
+   <footer>
+        <?php include('includes/footer.php');?>
+   </footer>
+</body>
+
+</html>
