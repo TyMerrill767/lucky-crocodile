b35dc3afa9b60b188d6aa85b280315a8c68ee5b9
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-15 13:58:56 -0600

    Adding link to font-awesome style sheet for icons.

--- includes/head.php
+++ includes/head.php
@@ -2,4 +2,5 @@
 <meta name="viewport" content="width=device-width initial-scale=1.0">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="css/owl.theme.css">
