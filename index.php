8e5e6fbe2f40a8ca62354b62d6eb2c3a972d2888
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-16 15:10:55 -0600

    Replacing static child image with image carousel.

--- index.php
+++ index.php
@@ -9,19 +9,30 @@
         <?php include('includes/header.php');?>
    </header>
 
-   <main role="main">
+   <main role="main" id="home-page-main">
 
-        <h1>Toddler Neglect in Vehicles</h1>
+        <h1>Unattended Toddlers in Vehicles</h1>
 
-        <p>Each year, an average of 37 children die due to vehicular heat stroke. Over 100 die due to back- and front-overs. Others die from the vehicle accidentally being set in motion, strangulation by power windows, choking on small objects, and more. One by one, these young lives were ended before their time. But what makes them especially tragic is that each and every one of them was preventable.</p>
+        <p class="home-page-content">Each year, an average of 37 children die due to vehicular heat stroke. Over 100 die due to back- and front-overs. Others die from the vehicle accidentally being set in motion, strangulation by power windows, choking on small objects, and more. One by one, these young lives were ended before their time. But what makes them especially tragic is that each and every one of them was preventable.</p>
 
-        <img src="images/child.png" alt="Child">
+        <div id="slide-area">
+            <div id="owl-demo" class="owl-carousel owl-theme">
 
-        <p>It isn't a matter of being a good or bad parent, or being an irresponsible person; all it takes is a momentary lapse of judgment on the part of an otherwise loving caretaker to endanger the life of a small child in a vehicle. This is because there are so many dangers lurking in and around a vehicle, both obvious and hidden, that can claim a child's life before one has time to realize it.</p>
+                  <div class="item"><img src="images/coffee.png" alt="Slide"></div>
+                  <div class="item"><img src="images/milk.png" alt="Slide"></div>
+                  <div class="item"><img src="images/money.png" alt="Slide"></div>
+                  <div class="item"><img src="images/Rx.png" alt="Slide"></div>
+                  <div class="item"><img src="images/wine.png" alt="Slide"></div>
+                  <div class="item"><img src="images/money_baby.png" alt="Slide"></div>
 
-        <p>So how can you avoid these tragedies? Read on to learn about the dangers and solutions to leaving a child alone in a vehicle, and how you can take action to avoid repeating these unfortunate mistakes before they happen.</p>
+            </div>
+        </div>
 
-       <a href="dangers.php" class="next-page-link">Dangers of leaving your child in a vehicle >></a>
+        <p class="home-page-content">It isn't a matter of being a good or bad parent, or being an irresponsible person; all it takes is a momentary lapse of judgment on the part of an otherwise loving caretaker to endanger the life of a small child in a vehicle. This is because there are so many dangers lurking in and around a vehicle, both obvious and hidden, that can claim a child's life before one has time to realize it.</p>
+
+        <p class="home-page-content">So how can you avoid these tragedies? Read on to learn about the dangers and solutions to leaving a child alone in a vehicle, and how you can take action to avoid repeating these unfortunate mistakes before they happen.</p>
+
+       <a href="dangers.php" class="next-page-link">Dangers of leaving your child in a vehicle &gt;&gt;</a>
    </main>
 
    <footer>
