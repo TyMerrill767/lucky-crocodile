384e34954022c58ddf021074db9e834194046b3e
TyMerrill767 <tyler_merrill2011@hotmail.com>
2015-12-18 13:53:02 -0600

    Adding PHP function to nav list.

--- includes/header.php
+++ includes/header.php
@@ -2,11 +2,11 @@
 <a href="index.php"><p id="tagline">Drivers Opposed to Neglecting Toddlers</p></a>
 <nav role="navigation" id="menu">
     <ul>
-        <li><a href="index.php" class="nav home">Home</a></li>
-        <li><a href="dangers.php" class="nav dangers">Dangers</a></li>
-        <li><a href="prevention.php" class="nav prev">Prevention</a></li>
-        <li><a href="action.php" class="nav action">Take Action</a></li>
-        <li><a href="about.php" class="nav about">About Us</a></li>
-        <li><a href="resources.php" class="nav resources">Resources</a></li>
+        <li><a <?php if ($thisPage=="Home") echo " id=\"home-active\""; ?>href="index.php" class="nav home">Home</a></li>
+        <li><a <?php if ($thisPage=="Dangers") echo " id=\"dangers-active\""; ?>href="dangers.php" class="nav dangers">Dangers</a></li>
+        <li><a <?php if ($thisPage=="Prevention") echo " id=\"prev-active\""; ?>href="prevention.php" class="nav prev">Prevention</a></li>
+        <li><a <?php if ($thisPage=="Take Action") echo " id=\"action-active\""; ?>href="action.php" class="nav action">Take Action</a></li>
+        <li><a <?php if ($thisPage=="About Us") echo " id=\"about-active\""; ?>href="about.php" class="nav about">About Us</a></li>
+        <li><a <?php if ($thisPage=="Resources") echo " id=\"resources-active\""; ?>href="resources.php" class="nav resources">Resources</a></li>
     </ul>
 </nav>
