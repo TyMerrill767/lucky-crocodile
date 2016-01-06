add569a9a8b9154ad16d04cb634441879132a53c
TyMerrill767 <tyler_merrill2011@hotmail.com>
2016-01-06 08:20:56 -0600

    Updating home page content.

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
         <h1>Unattended Toddlers in Vehicles</h1>
 
-        <p class="home-page-content">Each year, an average of 37 children die due to vehicular heat stroke. Over 100 die due to back- and front-overs. Others die from the vehicle accidentally being set in motion, strangulation by power windows, choking on small objects, and more. One by one, these young lives were ended before their time. But what makes them especially tragic is that each and every one of them was preventable.</p>
+        <p class="home-page-content">Each year, an average of 37 children die due to vehicular heat stroke. Over 100 die due to back-overs and front-overs. Others die from the vehicle accidentally being set in motion, strangulation by power windows, choking on small objects, poisoning, and more. What makes these child deaths especially tragic is that each and every one of them was preventable.</</p>
 
         <div id="slide-area">
             <div id="owl-demo" class="owl-carousel owl-theme">
@@ -29,9 +29,9 @@
             </div>
         </div>
 
-        <p class="home-page-content">It isn't a matter of being a good or bad parent, or being an irresponsible person; all it takes is a momentary lapse of judgment on the part of an otherwise loving caretaker to endanger the life of a small child in a vehicle. This is because there are so many dangers lurking in and around a vehicle, both obvious and hidden, that can claim a child's life before one has time to realize it.</p>
+        <p class="home-page-content">It isn&rsquo;t a matter of being a bad parent or being an irresponsible person. All it takes is a momentary lapse of judgment on the part of an otherwise loving caretaker to endanger the life of a small child in a vehicle. With regard to children, there are so many dangers with a vehicle, obvious and hidden, inside and out, that can claim a child&rsquo;s life before one has time to realize it.</p>
 
-        <p class="home-page-content">So how can you avoid these tragedies? Read on to learn about the dangers and solutions to leaving a child alone in a vehicle, and how you can take action to avoid repeating these unfortunate mistakes before they happen.</p>
+        <p class="home-page-content">How can we avoid these tragedies? Read further to learn about the dangers and solutions to leaving a child alone in a vehicle and how you can take action to avoid repeating these unfortunate mistakes before they happen.</p>
 
        <a href="dangers.php" class="next-page-link">Dangers of leaving your child in a vehicle &gt;&gt;</a>
    </main>
