4551e528a2f863ef4b0f9aadcec273d753b6184d
TyMerrill767 <tyler_merrill2011@hotmail.com>
2016-01-07 09:00:14 -0600

    Updating content.

--- prevention.php
+++ prevention.php
@@ -12,37 +12,56 @@
 
    <main role="main">
         <h1>Prevention and Precautions</h1>
-        <p>So now that you know the dangers, what can you do to prevent tragedy?</p>
+        <p>You know the dangers. How do you prevent tragedy?</p>
+        <h3>INSIDE THE VEHICLE</h3>
 
-        <section>
-        <h2>Inside the Vehicle</h2>
-        <img src="images/under_seat.png" alt="Under Seat Image" class="resize">
+        <h4>Heat Stroke/Hypothermia:</h4>
+        <p>Position your child&rsquo;s car seat on the passenger side to see your child easier.</p>
+        <p>Stage you car with cues to help remember your child:</p>
         <ul>
-            <li>Clean your car regularly – especially of small objects such as trinkets, toys, or stray food items that may have fallen into hidden spaces; be sure to look under seats and in storage areas</li>
-            <li>Choose foods that are easily chewed and swallowed to prevent choking, or cut foods into small pieces and remove things such as seeds and pits, and never leave a child unattended while eating</li>
-            <li>Place the child’s car seat so it is not directly behind the driver’s seat</li>
-            <li>Place a visual cue in the front seat, such as a diaper bag or stuffed animal, to remind yourself that the child is in the back seat</li>
+        <li>Keep a stuffed animal in the car seat at all times. When you buckle baby in, place it on the dash or in the passenger seat.</li>
+        <li>Keep important items you will need in the backseat with baby.</li>
+        <li> Keep your vehicle stocked with toys &lpar;not small enough to be choked on&rpar; to keep your child awake and giving auditory prompts of their presence.</li>
         </ul>
-       </section>
+        <p>Many devices and apps are now available to help you remember your child.</p>
 
-        <section>
-            <h2>Outside the Vehicle</h2>
-            <img src="images/suv_image.png" alt="Car Image" class="resize">
-            <ul>
-                <li>Always bring your child with you when you exit the vehicle – no amount of time is safe to leave your child unattended in the vehicle, even for “just a minute”, and it is illegal to do so in some states</li>
-                <li>Be sure that you lock the vehicle’s doors and trunk after exiting with your child – even when parked and at home, a vehicle can endanger a child, as they can become trapped within it if left open</li>
-                <li>Instruct children that the vehicle is not a safe place to play</li>
-            </ul>
-       </section>
+        <h4>Choking:</h4>
+        <p>Clean your vehicle regularly &ndash; especially of small objects such as trinkets, toys, or stray food items that may have fallen into hidden spaces; be sure to look under seats and in storage areas.</p>
+        <p>You should never leave a small child unattended while they are eating.</p>
+        <p>Children should never eat in the car.</p>
+        <p>If your child must eat in your vehicle, remain parked and never leave them unattended.</p>
+        <p>Feed your child at home before or after your errands.</p>
+        <p>Keep your vehicle clean and free of loose small items.</p>
 
-       <section>
-           <h2>In General</h2>
+        <h4>Front-overs/Back-overs:</h4>
+        <p>Average blindzone: 15-25 feet &lpar;shorter drivers have a greater blindzone&rpar;</p>
+        <p>Teach your children that any vehicle can move</p>
+        <p>Keep toys out of the driveway</p>
+        <p>Know where your children are before moving a vehicle</p>
 
-           <ul>
-               <li>Familiarize yourself with life-saving techniques such as the Heimlich Maneuver, child cardiopulminary resuscitation (CPR), and other first-aid measures, and call 911 for emergencies</li>
-               <li>Be sure your child's caretaker(s) are aware of the dangers and solutions, and always have a plan of action in case of emergency</li>
-               <li>Be aware of your child's behavior and condition: if they are acting strange or confused, or if they are exhibiting nausea, dizziness, sudden cessation of sweating, or quick/slow pulse, they may be suffering heat stroke</li>
-           </ul>
+        <h4>Vehicle accidentally set in motion:</h4>
+        <p>Shut the vehicle off and lock it when you leave it</p>
+        <p>Keep your car locked when not in use</p>
+        <p>Keep keys out of reach of children</p>
+
+        <h4>Power Windows:</h4>
+        <p>When children are riding, engage the child safety lock</p>
+        <p>Shut the vehicle off and lock it when you leave it</p>
+        <p>Teach children that power windows are dangerous</p>
+
+        <h4>Trunk Entrapment</h4>
+
+        <h4>Abduction</h4>
+
+        <h4>Poison</h4>
+
+        <h3>OUTSIDE THE VEHICLE</h3>
+        <p>Always bring your child with you when you exit the vehicle. No amount of time is safe to leave your child unattended, even for &ldquo;just a minute&rdquo;. It is illegal to do so in most states.</p>
+        <p>Be sure that your vehicle&rsquo;s doors and trunk remain closed and locked, even when parked at home. Children love to play hide-and-seek and other fun games. A vehicle may seem to them an ideal hiding spot, a stagecoach, a castle, or many other things. A vehicle can quickly become dangerous, as the child can easily become trapped within it. Instruct children that the vehicle is not a safe place to play.</p>
+        <p>Familiarize yourself with life-saving techniques such as the Heimlich Maneuver, child cardiopulmonary resuscitation &lpar;CPR&rpar;, and other first-aid measures, and call 911 for emergencies.</p>
+        <p>Make caretakers aware of the dangers and solutions, and always have a plan of action in case of emergency.</p>
+        <p>Greatest of all: YOU KNOW YOUR CHILD. If they are exhibiting abnormal behavior or exhibiting any of the signs of illness or injury, call the doctor or emergency services immediately!</p>
+
        </section>
 
    <a href="action.php" class="next-page-link">Find out how you can take action &gt;&gt;</a>
