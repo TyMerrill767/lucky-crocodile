e0985a3517992f2d9273f07db106ddad018d9847
TyMerrill767 <tyler_merrill2011@hotmail.com>
2016-01-07 08:59:57 -0600

    Updating content.

--- action.php
+++ action.php
@@ -13,13 +13,14 @@
    <main role="main">
        <h1>Take Action</h1>
         <img src="images/take_action.png" alt="Three Circles Child Safety">
-        <p>As someone with a personal connection to the field of child vehicle safety, be it through your own experience or that of a loved one, you are in an exclusive position to offer hope, understanding and crucial information to others. At the same time, it is important to carefully consider the potential impact of your message on the community, as well the effects of self-discovery on your personal well-being and for the well being of your children. </p>
+        <p>As someone with a personal connection to the field of child safety, be it through your own experience or that of a loved one, you are in an exclusive position to offer hope, understanding and crucial information to others. At the same time, it is important to carefully consider the potential impact of your message on the community, as well the effects of self-discovery on your personal well-being and for the well being of your children.</p>
 
-        <p>In the world of parenting, danger awaits around every corner. You have to be willing to put in the effort to protect your children from it. You are their guardian, their protector, YOU are responsible for what happens to your child so use what you now know about child vehicle safety and make a difference.</p>
+        <p>In the world of parenting, danger awaits around every corner. You have to be willing to put in the effort to protect your children. You are their guardian, their protector. YOU are responsible for what happens to your child so use what you now know about child vehicle safety and make a difference.</p>
 
-        <p>If you witness something that doesn’t follow child vehicle safety protocol do something. For example, if you witness someone lock their infant in the car while they go run errands notify the proper authorities immediately and proceed to break the window opposite of the infant to get it out. Be sure to wait for the authorities to arrive while you hold the infant. Always make sure to lock your car and keep your keys out of reach of children.</p>
+        <p>If you witness something that doesn&rsquo;t follow child vehicle safety protocol do something. If you witness someone leave an infant or toddler in a vehicle notify authorities immediately. If they don&rsquo;t return promptly, break the window opposite of the infant to get them out. Be sure to wait for the authorities to arrive while you hold the infant/toddler. Always make sure to lock your car and keep your keys out of reach of children.</p>
 
-        <p>Too many tragedies happen every year that can easily be prevented if the parties involved know proper child vehicle safety precautions. Everyone should have at the very least a basic understanding of child vehicle safety. It’s encouraged that you inform your friends and family with children about proper child vehicle safety and inform them of the risks that surround their vehicles. Who knows, you may just save a life.</p>
+        <p>Too many tragedies happen each year that can easily be prevented if the parties involved knew proper child vehicle safety precautions. Everyone should have at the very least a basic understanding of child vehicle safety. It&rsquo;s encouraged that you inform your friends and family with children about proper child vehicle safety and inform them of the risks that surround their vehicles. Who knows, you may just save a life.</p>
+
    </main>
 
    <footer>
