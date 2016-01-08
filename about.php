dd814e04206c28cf973b5137efac3f2ce7127c07
TyMerrill767 <tyler_merrill2011@hotmail.com>
2016-01-08 12:35:01 -0600

    Ameding page headers to be completely contained in header.php.

--- about.php
+++ about.php
@@ -6,10 +6,7 @@
 <?php include('includes/head.php');?>
 </head>
 <body>
-   <header role="banner">
-        <?php include('includes/header.php');?>
-   </header>
-
+   <?php include('includes/header.php');?>
    <main role="main">
         <h1>About Us</h1>
 
