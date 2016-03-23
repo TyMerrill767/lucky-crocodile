37d6761d06d8545b512b936fc3b9f983c303462e
Brian Richardson <brisquatch@gmail.com>
2016-03-23 14:27:12 -0500

    text changes to danger.php

--- dangers.php
+++ dangers.php
@@ -1,143 +1,198 @@
 <!DOCTYPE html>
 <?php $thisPage="Dangers"; ?>
-<html lang="en">
-<head>
-<title>D.O.N.T. -- Dangers of Leaving Your Child Unattended in a Vehicle</title>
-<?php include('includes/head.php');?>
-</head>
-<body>
-   <?php include('includes/header.php');?>
+	<html lang="en">
 
-   <main role="main">
-        <h1>Dangers</h1>
-        <p>There are many things in any vehicle that can endanger a child, not all of them obvious. Listed below are the many dangers a child can encounter if left in a vehicle.</p>
-        <img src="images/interior1.png" alt="Car Interior 1" class="resize">
-        <h2>Heat Stroke</h2>
-        <h3>How It Happens:</h3>
-        <ul>
-            <li>Parent is in a rush and due to the baby being asleep forgets their child.</li>
-            <li>Parent isn&rsquo;t aware of the risks of leaving their child in the car and leaves their child there while they go shop or run errands.</li>
-        </ul>
-        <h3>Facts:</h3>
-        <ul>
-            <li>About 87% of children who have perished from vehicular heat stroke are age 3 and younger.</li>
-            <li>About 54% of vehicular heat stroke deaths involve children age one and under.</li>
-            <li>The average number of U.S. child vehicular heat stroke is 37 a year. That&rsquo;s about one every nine days.</li>
-            <li>In the first 10 minutes the temperature of a car increases by about 80%!</li>
-            <li>There are cases where children have died in cars while the outside temperature was as low as 60 degrees Fahrenheit.</li>
-            <li>When a human&rsquo;s body temp reaches 104 degrees Fahrenheit internal organs have a tendency to shut down. Pretty safe to say you kind of need those working.</li>
-            <li>At 107 degrees Fahrenheit children die.</li>
-            <li>With an outside temperature as low as 60 degrees your car can still heat up to well over 100.</li>
-        </ul>
-        <h3>Symptoms of Heat Stroke:</h3>
-        <ul>
-            <li>Hot, red, and dry or moist skin.</li>
-            <li>No sweating.</li>
-            <li>A slow weak pulse or a rapid strong pulse.</li>
-            <li>A throbbing headache.</li>
-            <li>Dizziness.</li>
-            <li>Nausea.</li>
-            <li>Confusion.</li>
-            <li>Acting strangely or being grouchy.</li>
-        </ul>
-        <h3>Prevention Tips:</h3>
-        <ul>
-            <li>Position your child&rsquo;s car seat so that it isn&rsquo;t directly behind your drivers&rsquo; seat.</li>
-            <li>By placing the car seat on the passenger side it allows parents to see their child easier.</li>
-            <li>Put something of importance in your back seat before you start driving. That way you are forced to turn around and see your child.</li>
-            <li>You can also put something in the back to keep your child from falling asleep like a toy or a diaper bag. If they fall asleep they can't give you auditory clues.</li>
-            <li>Try and get into the habit of looking in your back seats before you lock your car.</li>
-        </ul>
-        <img src="images/interior2.png" alt="Car Interior 2" class="resize">
-        <h2>Choking</h2>
-        <h3>How It Happens:</h3>
-        <ul>
-            <li>Small objects (such as a pencil, loose change, seeds, etc.) get left or forgotten in car only to have baby find them and stick them in his/her mouth.</li>
-        </ul>
-        <h3>Facts:</h3>
-        <ul>
-            <li>Choking is the fourth leading cause of unintentional death in children under the age of five.</li>
-            <li>Children younger than five are at the greatest risk of suffering a choking related injury or death.</li>
-            <li>Just about any small item you can think of is a choking hazard.</li>
-            <li>The most common cause of choking that is nonfatal in young children is food.</li>
-            <li>At least one child dies from choking on food every five days in the U.S., and more than 10,000 children are taken to a hospital emergency room each year for injuries caused by food choking.</li>
-        </ul>
-        <h3>Symptoms of Choking:</h3>
-        <ul>
-            <li>Struggling to breathe (gasping).</li>
-            <li>Coughing.</li>
-            <li>Gaggging.</li>
-            <li>Bluish lips or skin.</li>
-            <li>High Pitched voice.</li>
-        </ul>
-        <h3>Prevention Tips:</h3>
-        <ul>
-            <li>You should never leave a small child unattended while they are eating. Direct supervision is needed.</li>
-            <li>Children shouldn&rsquo;t eat while walking, riding in a car or playing.</li>
-            <li>Cut your child&rsquo;s food into small pieces, removing seeds and pits.</li>
-            <li>Cook vegetables to soften them.</li>
-            <li>Chew food properly and model some safe eating habits for your kids.</li>
-            <li>Make sure your children have a calm, unhurried meal.</li>
-            <li>Clean the inside of your car thoroughly before hand if you are going to have a child sit in it.</li>
-        </ul>
-        <h2>Front-overs/Back-overs</h2>
-        <h3>How It Happens:</h3>
-        <ul>
-            <li>A frontover is when a driver doesn&rsquo;t see a child in front of the vehicle and goes forward.</li>
-            <li>A Backover is when a driver doesn&rsquo;t see a child behind the vehicle and backs up.</li>
-        </ul>
-        <h3>Facts:</h3>
-        <ul>
-            <li>Of all nontraffic fatalities involving children under 15 years old from 2006 to 2010 30% of those were from Frontovers while 34% were from Backovers.</li>
-            <li>Frontovers and Backovers can happen in any vehicle because all vehicles have a blind zone which is the area in front of a vehicle where you can&rsquo;t see from the driver&rsquo;s seat.</li>
-            <li>The predominant age of backover victims is one year old.</li>
-            <li>Over 60% of back up incidents involved larger size vehicle such as a truck, van, or SUV.</li>
-            <li>Sadly in a little over 70% of these incidents, a close relative or parent are usually the ones behind the wheel.</li>
-        </ul>
-        <h3>Prevention Tips:</h3>
-        <ul>
-            <li>Completely walk around and behind your vehicle before moving it.</li>
-            <li>You can always measure the size of your blind zone in the front and back of your vehicle. Most drivers cannot see a young child six to eight feet in front or back of their vehicle.</li>
-            <li>Teach your kids that there is always a chance for a parked car to move and just because they can see the car doesn&rsquo;t mean the car can see them.</li>
-            <li>Keep all sports equipment and toys out of the driveway.</li>
-            <li>Know where your kids are before you go anywhere.</li>
-        </ul>
-        <img src="images/interior3.png" alt="Car Interior 3" class="resize">
-        <h2>Vehicle Accidentally Getting Set In Motion</h2>
-        <h3>How It Happens:</h3>
-        <ul>
-            <li>Parent leaves keys where child can reach them. Child then proceeds to get in car and set it in motion.</li>
-        </ul>
-        <h3>Facts:</h3>
-        <ul>
-            <li>Of all nontraffic fatalities involving children between 2006 and 2010 vehicle in motion made up 5% of all fatalities.</li>
-        </ul>
-        <h3>Prevention Tips:</h3>
-        <ul>
-            <li>Always make sure your car is locked and keep your keys out of reach from children.</li>
-        </ul>
-        <h2>Power Windows</h2>
-        <h3>How it Happens:</h3>
-        <ul>
-            <li>Kid is hanging out the window and accidentally rolls it up by hitting the button.</li>
-        </ul>
-        <h3>Facts:</h3>
-        <ul>
-            <li>Power windows in vehicles have injured or killed thousands of children.</li>
-            <li>The amount of upward force a power window exerts up is between 30 and 80 pounds. It only takes 22 pounds of force to injure and potentially suffocate an infant.</li>
-        </ul>
-        <h3>Prevention Tips:</h3>
-        <ul>
-            <li>Most cars nowadays have the ability to lock window functionality so make sure your windows are always locked.</li>
-            <li>Always make sure your children are properly secured in their set while driving.</li>
-        </ul>
+	<head>
+		<title>D.O.N.T. -- Dangers of Leaving Your Child Unattended in a Vehicle</title>
+		<?php include('includes/head.php');?>
+	</head>
 
-       <a href="prevention.php" class="next-page-link">How to prevent these dangers &gt;&gt;</a>
-   </main>
+	<body>
+		<?php include('includes/header.php');?>
 
-   <footer>
-        <?php include('includes/footer.php');?>
-   </footer>
-</body>
+			<?php include('includes/dangersnav.php');?>
 
-</html>
+				<main role="main" class="main_right dangers_main">
+					<h1>Dangers</h1>
+					<p class="dangers_p">There are many things in any vehicle that can endanger a child, not all of them obvious. Toddlers are naturally playful and curious. They love to explore all of the different crevices, containers, and controls inside of your vehicle.
+						<br>
+						<br> Listed below are the many dangers a child can encounter if left in or near a vehicle unattended.</p>
+
+					<img src="images/interior3.png" alt="Car Interior 3" class="resize float_img">
+
+					<div class="danger_facts">
+						<div class="how">
+							<h2 id="heatstroke">Heat Stroke</h2>
+							<img src="images/heatstrokeDanger.png" alt="Heatstroke interior" class="resize float_right">
+							<h3>How it Happens:</h3>
+							<p>Sleeping babies are quiet. A rushed parent is likely to be focused on many things. Especially when caring for your child at the time is outside of your normal routine, a sleeping child can be forgotten.</p>
+							<p>Parents are not always aware of these risks and leave their child while they quickly go inside a location on an errand. &ldquo;It was just for a minute,&rdquo; is the most common response to one of these tragedies.</p>
+						</div>
+						<div class="facts">
+							<h3>Facts:</h3>
+							<p>Deaths of children age 3 or younger: 87%</p>
+							<p>Deaths of children age 1 and under: 54%</p>
+							<p>Average number of child vehicular heat strokes (U.S. alone): 37</p>
+							<p>10 minutes = 80% increase in temperature</p>
+							<p>60&deg; outside can equal over 100&amp;deg; inside of a car</p>
+							<p>104&deg; : The temperature that the internal organs begin to shut down</p>
+							<p>107&deg; = DEATH</p>
+						</div>
+						<div class="symptoms">
+							<h3>Symptoms of Heat Stroke:</h3>
+							<ul>
+								<li>Hot and/or red skin</li>
+								<li>Irregular sweating</li>
+								<li>An irregular pulse</li>
+								<li>Headache</li>
+								<li>Dizziness</li>
+								<li>Nausea</li>
+								<li>Confusion</li>
+								<li>Acting strangely or being grouchy</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="danger_facts">
+						<div class="how">
+							<h2 id="choking">Choking</h2>
+							<img src="images/chokingDanger.png" alt="choking interior" class="resize float_right">
+							<h3>How it Happens:</h3>
+							<p>Small objects (pencils, loose change, paper clips, food items) get left in car only to have baby find them and stick them in his/her mouth.</p>
+
+							<h3>Facts:</h3>
+							<p>4<sup>th</sup> leading cause of death in children under 5</p>
+							<p>ANY small item can be a risk</p>
+							<p>Most common: food</p>
+							<p>1 child chokes on food every 5 days</p>
+							<p>10,000: Number of children taken to the ER each year for choking</p>
+						</div>
+						<div class="symptoms">
+							<h3>Symptoms of Choking:</h3>
+							<ul>
+								<li>Struggling to breathe (gasping)</li>
+								<li>Coughing</li>
+								<li>Gagging</li>
+								<li>Bluish lips or skin</li>
+								<li>High-pitched voice</li>
+							</ul>
+						</div>
+					</div>
+
+					<img src="images/interior1.png" alt="Car Interior 1" class="resize float_imgs">
+
+					<div class="danger_facts">
+						<div class="how">
+							<h2 id="poison">Poison</h2>
+							<img src="images/poisonDanger.png" alt="poison interior" class="resize float_right">
+							<h3>How it Happens:</h3>
+							<p>A child left alone in your car after a long day of shopping has access to many items you may not think are dangerous.</p>
+
+							<h3>Facts:</h3>
+							<p>Grocery bags can be filled with items that can make your child sick and can even be toxic, such as:</p>
+							<ul>
+								<li>Cleansers/Bleach</li>
+								<li>Lotions/Hand sanitizer</li>
+								<li>Tobacco products</li>
+								<li>Adult beverages</li>
+								<li>Automotive products (De-icer, oil, flares, etc.)</li>
+								<li>Food items that have been sitting in a vehicle for extended periods (Mold/Bacteria can be fatal to a young child&rsquo;s weak immune system)</li>
+							</ul>
+							<p>Think that some of these sound outrageous or extreme? Remember, a toddler is apt to put ANYTHING in his/her mouth.</p>
+						</div>
+						<div class="symptoms">
+							<h3>Symptoms of Poisoning:</h3>
+							<ul>
+								<li>Behavior changes (restlessness, crankiness)</li>
+								<li>Diarrhea</li>
+								<li>Dizziness</li>
+								<li>Drowsiness</li>
+								<li>Fatigue</li>
+								<li>Headache</li>
+								<li>Loss of appetite</li>
+								<li>Minor skin or eye irritation</li>
+								<li>Nausea or upset stomach</li>
+								<li>Passing cough (cough that comes and goes)</li>
+								<li>Soreness or stiffness in the joints</li>
+								<li>Thirst</li>
+							</ul>
+							<p>&dash; See more at: <a href="http://www.healthcommunities.com/poisoning/symptoms.shtml#sthash.gDiI9Zt6.dpuf">healthcommunities.com</a></p>
+						</div>
+					</div>
+
+					<img src="images/interior2.png" alt="Car Interior 2" class="resize float_imgs">
+
+					<div class="danger_facts">
+						<div class="how">
+							<h2 id="front_back">Front&dash;overs/Back&dash;overs</h2>
+							<img src="images/backoverDanger.png" alt="Backover interior" class="resize float_right">
+							<h3>How It Happens:</h3>
+							<p>Children love to play and hide around all kinds of different obstacles and to them a vehicle is no more unsafe than a tree in the front yard.</p>
+							<p>A front&dash;over or back&dash;over occurs when a driver doesn't see a child in front or behind and sets the vehicle in motion resulting in injury or death.</p>
+
+							<h3>Facts:</h3>
+							<p>Of all non-traffic fatapties involving children under 15 years old from 2006 to 2010 30% of those were from front-overs while 34% were from back-overs.</p>
+							<p>Front-overs and back-overs can happen in any vehicle because all vehicles have a bpndzone, an area around a vehicle that can&rsquo;t be seen from the driver&rsquo;s seat.</p>
+							<p>The predominant age of back-over victims is one year old.</p>
+							<p>Over 60% of back up incidents involved larger size vehicle such as a truck, van, or SUV.</p>
+							<p>Sadly in a pttle over 70% of these incidents, a close relative or parent are usually the ones behind the wheel.</p>
+						</div>
+					</div>
+
+					<div class="danger_facts">
+						<div class="how">
+							<h2 id="abduction">Abduction</h2>
+							<img src="images/abductionDanger.png" alt="abduction danger" class="resize float_left">
+							<h3>How it Happens:</h3>
+							<p>Children left alone in a vehicle are vulnerable to being taken.</p>
+							<p>Unattended children have access to door locks. Easily influenced, a stranger can coerce them into unlocking the vehicle.</p>
+
+							<h3>Facts:</h3>
+							<p>A stranger doesn&rsquo;t need to forcefully take the child. Persuading them to exit the vehicle with temptations is just as much a threat as breaking a window and taking the child.</p>
+							<p>It can only take a few seconds for your child to be taken.</p>
+							<p>Additionally, a child can be unintentionally abducted should a stranger attempt to steal your vehicle.</p>
+						</div>
+					</div>
+
+					<div class="danger_facts">
+						<div class="how">
+							<h2 id="in_motion">Vehicle Accidentally Set in Motion</h2>
+							<img src="images/motionDanger.png" alt="controls image" class="resize float_right">
+							<h3>How it Happens:</h3>
+							<p>Unattended children in a vehicle can free themselves from car seats, gaining access to controls. Re&dash;enacting a movie racecar or spaceship scene can put them at risk of endangering their or another's life.</p>
+
+							<h3>Facts:</h3>
+							<p>Window controls are easy to operate.</p>
+							<p>Gear&amp;shifts are usually easily moved levers.</p>
+							<p>A child doesn&rsquo;t have to be targeting a control to activate it. They may simply grab it or use it for support while moving about the vehicle.</p>
+							<p>Of all non-traffic fatalities involving children between 2006 and 2010 vehicle in motion made up 5% of all fatalities.</p>
+						</div>
+					</div>
+
+					<div class="danger_facts">
+						<div class="how">
+							<h2 id="power_windows">Injury/Strangulation from Power Windows</h2>
+							<img src="images/windowDanger.png" alt="window image" class="resize float_left">
+							<h3>How it Happens:</h3>
+							<p>Curious and playful children want to touch everything and operate moving pieces. Left unattended, they can activate many of the vehicle&rsquo;s automatic features. Innocent playtime can quickly become harmful or even deadly.</p>
+
+							<h3>Facts:</h3>
+							<ul>
+								<li>Power windows can easily break small bones and cause strangulation.</li>
+								<li>Thousands of children have been harmed or killed by power windows.</li>
+								<li>The amount of upward force a power window exerts up is between 30 and 80 pounds.</li>
+								<li>It only takes 22 pounds of force to injure and potentially suffocate an infant.</li>
+							</ul>
+						</div>
+					</div>
+
+					<a href="prevention.php" class="action_btn">How to prevent these dangers >></a>
+					<a href="#top" class="action_btn">Back to Top</a>
+				</main>
+
+				<footer>
+					<?php include('includes/footer.php');?>
+				</footer>
+	</body>
+
+	</html>
