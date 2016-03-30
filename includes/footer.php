5fd5d671e98e12d419883bdb1edb8dae767024ee
TyMerrill767 <tyler_merrill2011@hotmail.com>
2016-03-30 17:57:48 -0500

    Updating styling for Back To Top link.

--- includes/footer.php
+++ includes/footer.php
@@ -27,6 +27,17 @@
 	});
 </script>
 
+<script>
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+</script>
 
 <footer role="contentinfo">
 	<p>&copy;
