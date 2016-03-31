9b7f1bf5cebe26b5e3b6178204c5287043316199
TyMerrill767 <tyler_merrill2011@hotmail.com>
2016-03-30 19:41:00 -0500

    Re-adding code to footer post-crash.

--- includes/footer.php
+++ includes/footer.php
@@ -1,30 +1,39 @@
+<footer>
+<div id="bpa-info">
+    <p>BPA Information:</p>
+    <p>Chapter: 03-0042</p>
+    <p>Theme: Toddler Neglect in Vehicles</p>
+    <p>School: Francis Tuttle Technology Center</p>
+    <p>City: Oklahoma City, OK</p>
+    <p>Year: 2016</p>
+</div>
+
 <script src="js/jquery.min.js"></script>
 <script src="js/jquery.slicknav.min.js"></script>
 <script src="js/owl.carousel.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
 <!--Initializes responsive menu functionality-->
 <script>
-	$(function () {
+	$(function(){
 		$('#menu').slicknav();
 	});
 </script>
 
 <script>
-	$(document).ready(function () {
+    $(document).ready(function() {
 
-		$("#owl-demo").owlCarousel({
+      $("#owl-demo").owlCarousel({
 
-			slideSpeed: 800,
-			paginationSpeed: 800,
-			singleItem: true,
-			autoPlay: 7000,
-			navigation: true,
-			navigationText: ["<i class='fa fa-arrow-circle-left'></i>", "<i class='fa fa-arrow-circle-right'></i>"],
-			stopOnHover: true,
+          slideSpeed: 800,
+          paginationSpeed: 800,
+          singleItem: true,
+          autoPlay: 7000,
+          stopOnHover: true,
+      });
 
-		});
+    });
 
-	});
+
 </script>
 
 <script>
@@ -37,24 +46,5 @@ $(window).scroll(function() {
 		$('a.back-to-top').fadeOut('slow');
 	}
 });
-</script>
-
-<footer role="contentinfo">
-	<p>&copy;
-		<?php
-		$startYear = 2015;
-		$thisYear = date('Y');
-		if ($startYear == $thisYear) {
-			echo $startYear;
-		} else {
-			echo "{$startYear}&ndash;{$thisYear}";
-		}
-	?>
-			D.O.N.T.</p>
-
-	<p>BPA Information:</p>
-
-	Chapter: 03&dash;0042 &#124; Theme: Toddler Neglect in Vehicles &#124; School: Francis Tuttle Technology Center &#124; City: Oklahoma City, OK &#124; Year: 2015</p>
-	<a href="#" class="go-top">Go Top</a>
-
+    </script>
 </footer>
